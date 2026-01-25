4412634d9c24f4b6534028d4950631a531767750
Mirela <169615295+iammirela@users.noreply.github.com>
2026-01-25 18:51:02 +0000

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,24 @@
 (() => {
-  // Theme switch
+  // Initialize theme
   const body = document.body;
+  const data = body.getAttribute("data-theme");
+  
+  const initTheme = (state) => {
+    if (state === "dark") {
+      body.setAttribute("data-theme", "dark");
+    } else if (state === "light") {
+      body.removeAttribute("data-theme");
+    } else {
+      localStorage.setItem("theme", data);
+    }
+  };
+  
+  // Initialize on page load
+  initTheme(localStorage.getItem("theme"));
+  
+  // Theme switch
   const lamp = document.getElementById("mode");
-
+  
   const toggleTheme = (state) => {
     if (state === "dark") {
       localStorage.setItem("theme", "light");
@@ -14,18 +30,21 @@
       initTheme(state);
     }
   };
-
-  lamp.addEventListener("click", () =>
-    toggleTheme(localStorage.getItem("theme"))
-  );
-
+  
+  if (lamp) {
+    lamp.addEventListener("click", () =>
+      toggleTheme(localStorage.getItem("theme"))
+    );
+  }
+  
   // Blur the content when the menu is open
   const cbox = document.getElementById("menu-trigger");
-
-  cbox.addEventListener("change", function () {
-    const area = document.querySelector(".wrapper");
-    this.checked
-      ? area.classList.add("blurry")
-      : area.classList.remove("blurry");
-  });
+  if (cbox) {
+    cbox.addEventListener("change", function () {
+      const area = document.querySelector(".wrapper");
+      this.checked
+        ? area.classList.add("blurry")
+        : area.classList.remove("blurry");
+    });
+  }
 })();
